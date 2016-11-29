6417c5436428fa4e316943087b37caa26454f5f8
Sebastiaan Poppen <sp.poppen@quicknet.nl>
2016-11-29 17:08:32 +0100

    create first models

---
+++ db/migrate/20161129160306_create_ideas.rb
@@ -0,0 +1,13 @@
+class CreateIdeas < ActiveRecord::Migration[5.0]
+  def change
+    create_table :ideas do |t|
+      t.string :name
+      t.string :image_url
+      t.string :video_url
+      t.string :string
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
